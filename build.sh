bd99e1e43486aa7d203dd637cb7274d58b867db3
Gouse Velluri <gousevelluri444@gmail.com>
2026-02-27 00:35:42 +0530

    Auto-create superuser during deployment

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies, collect static files, migrate database
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create superuser automatically
-python manage.py createsuperuser --no-input
+# Create superuser automatically using environment variables
+# Make sure these variables are set in Render Environment Variables:
+#   SUPERUSER_NAME, SUPERUSER_EMAIL, SUPERUSER_PASSWORD
+python manage.py createsuperuser \
+    --no-input \
+    --username "$SUPERUSER_NAME" \
+    --email "$SUPERUSER_EMAIL" \
+    || echo "Superuser already exists, skipping..."
